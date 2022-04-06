8c1cbb2ac672b263f3e4958d197b5de5b752b829
asouvanlasy <asouvanlasy@outlook.com>
2022-04-06 14:49:31 -0400

    IllegalAccessError when running JUnit test on DayTest.java

---
+++ src/main/tests/DayTest.java
@@ -0,0 +1,36 @@
+import com.example.weeklyschedule.Day;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DayTest {
+    private Day day;
+
+    @BeforeEach
+    void setUp() {
+        day = new Day("Your task", LocalDate.EPOCH);
+    }
+
+    @Test
+    void setTask() {
+        day.setTask("A string");
+        assertEquals("Some sort of string", day.getTask());
+    }
+
+    @Test
+    void setTaskInvalid() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            day.setTask("");
+        });
+    }
+
+    @Test
+    void setDate() {
+        day.setDate(LocalDate.EPOCH);
+        assertEquals(LocalDate.EPOCH, day.getDate());
+    }
+}
